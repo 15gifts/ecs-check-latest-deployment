06bc3ba464aee423167f7f1a5f180cf6a6b33cd4
Richard Green <richard.green@15gifts.com>
2026-05-15 10:40:49 +0100

    Amend biome formatting rules

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -6,54 +6,52 @@
  * variables following the pattern `INPUT_<INPUT_NAME>`.
  */
 
-import { describe, it, expect, vi, beforeEach } from "vitest";
-import * as core from "@actions/core";
-import * as main from "../src/main";
+import { describe, it, expect, vi, beforeEach } from "vitest"
+import * as core from "@actions/core"
+import * as main from "../src/main"
 
 describe("action", () => {
-	const debugMock = vi.spyOn(core, "debug").mockImplementation(() => {});
-	const errorMock = vi.spyOn(core, "error").mockImplementation(() => {});
-	const getInputMock = vi.spyOn(core, "getInput").mockImplementation(() => "");
-	const setFailedMock = vi
-		.spyOn(core, "setFailed")
-		.mockImplementation(() => {});
+  const debugMock = vi.spyOn(core, "debug").mockImplementation(() => {})
+  const errorMock = vi.spyOn(core, "error").mockImplementation(() => {})
+  const getInputMock = vi.spyOn(core, "getInput").mockImplementation(() => "")
+  const setFailedMock = vi.spyOn(core, "setFailed").mockImplementation(() => {})
 
-	beforeEach(() => {
-		vi.clearAllMocks();
-	});
+  beforeEach(() => {
+    vi.clearAllMocks()
+  })
 
-	it("debugs out an input", async () => {
-		// Set the action's inputs as return values from core.getInput()
-		getInputMock.mockImplementation((name) => {
-			switch (name) {
-				case "ecs-cluster":
-					return "test-cluster";
-				case "ecs-service":
-					return "test-service";
-				default:
-					return "";
-			}
-		});
+  it("debugs out an input", async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation((name) => {
+      switch (name) {
+        case "ecs-cluster":
+          return "test-cluster"
+        case "ecs-service":
+          return "test-service"
+        default:
+          return ""
+      }
+    })
 
-		await main.run();
+    await main.run()
 
-		expect(debugMock).toHaveBeenNthCalledWith(
-			1,
-			expect.stringContaining("Checking latest ECS deployment using"),
-		);
-	});
+    expect(debugMock).toHaveBeenNthCalledWith(
+      1,
+      expect.stringContaining("Checking latest ECS deployment using"),
+    )
+  })
 
-	it("sets a failed status", async () => {
-		// Set the action's inputs as return values from core.getInput()
-		getInputMock.mockImplementation(() => "");
+  it("sets a failed status", async () => {
+    // Set the action's inputs as return values from core.getInput()
+    getInputMock.mockImplementation(() => "")
 
-		await main.run();
+    await main.run()
 
-		// Verify that all of the core library functions were called correctly
-		expect(setFailedMock).toHaveBeenNthCalledWith(
-			1,
-			"ECS cluster not specified",
-		);
-		expect(errorMock).not.toHaveBeenCalled();
-	});
-});
+    // Verify that all of the core library functions were called correctly
+    expect(setFailedMock).toHaveBeenNthCalledWith(
+      1,
+      "ECS cluster not specified",
+    )
+    expect(errorMock).not.toHaveBeenCalled()
+  })
+})
--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,38 @@
-import * as core from "@actions/core";
+import * as core from "@actions/core"
 
-import { CommandOptions, execute } from "./utils/execute";
+import { CommandOptions, execute } from "./utils/execute"
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
 export async function run(): Promise<void> {
-	try {
-		const options: CommandOptions = {
-			dryRun: (core.getInput("dry-run") ?? "false") === "true",
-			ecsCluster: core.getInput("ecs-cluster", { required: true }),
-			ecsService: core.getInput("ecs-service", { required: true }),
-			awsRegion: core.getInput("aws-region", { required: true }),
-		};
+  try {
+    const options: CommandOptions = {
+      dryRun: (core.getInput("dry-run") ?? "false") === "true",
+      ecsCluster: core.getInput("ecs-cluster", { required: true }),
+      ecsService: core.getInput("ecs-service", { required: true }),
+      awsRegion: core.getInput("aws-region", { required: true }),
+    }
 
-		if (!options.ecsCluster) {
-			throw new Error("ECS cluster not specified");
-		}
-		if (!options.ecsService) {
-			throw new Error("ECS service not specified");
-		}
+    if (!options.ecsCluster) {
+      throw new Error("ECS cluster not specified")
+    }
+    if (!options.ecsService) {
+      throw new Error("ECS service not specified")
+    }
 
-		core.debug(
-			`Checking latest ECS deployment using ${JSON.stringify(
-				options,
-				null,
-				2,
-			)}...`,
-		);
+    core.debug(
+      `Checking latest ECS deployment using ${JSON.stringify(
+        options,
+        null,
+        2,
+      )}...`,
+    )
 
-		await execute(options);
-	} catch (error) {
-		// Fail the workflow run if an error occurs
-		if (error instanceof Error) core.setFailed(error.message);
-	}
+    await execute(options)
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
 }
